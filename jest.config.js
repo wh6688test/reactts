014f36b70024b6e84e465bcbcfe7df252bf742dd
Jenkins <jenkins@wens-mbp.attlocal.net>
2021-04-08 23:10:18 -0400

    adding dummy code coverage by default for jest tests

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest",
   },
   moduleNameMapper: {
-    "\\.(scss|css|jpg|png|gif)$": "<rootDir>/src/tests/file.mock.ts"
+    "\\.(scss|css|jpg|png|gif)$": "<rootDir>/src/tests/file.mock.js"
   },
   testMatch: [ 
     '**/src/**/?(*.)+(test).(js|ts|tsx)', 
@@ -14,11 +14,29 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   collectCoverage: true,
   coverageDirectory: "<rootDir>/test-coverage",
+  coverageThreshold: {
+    "global": {
+      "branches": 10,
+      "functions": 10,
+      "lines": 10,
+      "statements": -10
+    },
+    "./src/apps": {
+      "branches": 40,
+      "statements": 40,
+      "functions": 20
+    },
+    "./src/components": {
+      "statements": 20,
+      "functions": 20
+    }
+  },
   globals: {
     "ts-jest": {
       diagnostics: {
         warnOnly: true
       }
-    }
-  },
+    },
+    "__DEV__": true
+  }
 };
